7397214cf81764a96d644e6e86752fccb6156d5f
IvanTurchyn <ivan.turchyn@interia.pl>
2023-02-03 21:38:17 +0100

    Finally commit (added Tests) before Maven build app.

--- src/test/java/com/example/ManagementApp/controller/UserControllerTest.java
+++ src/test/java/com/example/ManagementApp/controller/UserControllerTest.java
@@ -1,74 +1,74 @@
 package com.example.ManagementApp.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
 import com.example.ManagementApp.model.User;
 import com.example.ManagementApp.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@ExtendWith(MockitoExtension.class)
-public class UserControllerTest {
-    @InjectMocks
-    private UserController userController;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class UserControllerTest {
     @Mock
     private UserService userService;
 
-    @Test
-    public void addUserTest() {
-        User user = new User();
-        user.setFirstName("test_user");
-        user.setLastName("test_password");
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void findAllUsers_ShouldReturnListOfUsers() {
-        User user1 = new User("John", "Doe", "john.doe@example.com");
-        User user2 = new User("Jane", "Doe", "jane.doe@example.com");
-        List<User> users = Arrays.asList(user1, user2);
-        when(userService.getAllUsers()).thenReturn(users);
-
-        List<User> response = userController.findAllUsers();
-
-        assertThat(response, is(users));
+    void addUser() {
+        User user = new User("firstName", "lastName", "email");
+        userController.addUser(user);
+        verify(userService, times(1)).saveUser(user);
     }
 
     @Test
-    public void findByEmail_ShouldReturnUser() {
-        User user = new User("John", "Doe", "john.doe@example.com");
-        when(userService.findByEmail("john.doe@example.com")).thenReturn(user);
-
-        User response = userController.findByEmail("john.doe@example.com");
-
-        assertThat(response, is(user));
+    void findAllUsers() {
+        List<User> expectedUsers = Arrays.asList(
+                new User("firstName1", "lastName1", "email1"),
+                new User("firstName2", "lastName2", "email2")
+        );
+        when(userService.getAllUsers()).thenReturn(expectedUsers);
+        List<User> actualUsers = userController.findAllUsers();
+        verify(userService, times(1)).getAllUsers();
+        assertEquals(expectedUsers, actualUsers);
     }
 
     @Test
-    public void updateUser_ShouldReturnUpdatedUser() {
-        User user = new User("John", "Doe", "john.doe@example.com");
-        String id = "2";
-        when(userService.updateUser(id, user)).thenReturn(user);
-
-        User response = userController.updateUser("1", user);
-
-        assertThat(response, is(user));
+    void findById() {
+        Optional<User> expectedUser = Optional.of(new User("firstName", "lastName", "email"));
+        when(userService.findById(anyString())).thenReturn(expectedUser);
+        Optional<User> actualUser = userController.findById("id");
+        verify(userService, times(1)).findById(anyString());
+        assertEquals(expectedUser, actualUser);
     }
 
     @Test
-    public void deleteUser_ShouldCallDeleteUserMethodFromUserService() {
-        userController.deleteUser("1");
+    void findByEmail() {
+        User expectedUser = new User("firstName", "lastName", "email");
+        when(userService.findByEmail(anyString())).thenReturn(expectedUser);
+        User actualUser = userController.findByEmail("email");
+        verify(userService, times(1)).findByEmail(anyString());
+        assertEquals(expectedUser, actualUser);
     }
 
+    @Test
+    void updateUser() {
+        User expectedUser = new User("firstName", "lastName", "email");
+        when(userService.updateUser(anyString(), any(User.class))).thenReturn(expectedUser);
+        User actualUser = userController.updateUser("id", expectedUser);
+        verify(userService, times(1)).updateUser(anyString(), any(User.class));
+        assertEquals(expectedUser, actualUser);
+    }
 }
--- src/test/java/com/example/ManagementApp/service/UserServiceTest.java
+++ src/test/java/com/example/ManagementApp/service/UserServiceTest.java
@@ -1,79 +1,70 @@
 package com.example.ManagementApp.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import com.example.ManagementApp.model.User;
 import com.example.ManagementApp.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-class UserServiceTest {
-
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
     @Mock
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @InjectMocks
-    UserService userService;
+    private UserService userService;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void saveUserTest() {
-        User user = new User("Jan", "Kowalski","test@example.com");
-        when(userRepository.save(any(User.class))).thenReturn(user);
+        User user = new User("firstName", "lastName", "email");
+        when(userRepository.save(user)).thenReturn(user);
+        User actualUser = userService.saveUser(user);
+        verify(userRepository, times(1)).save(user);
+        assertEquals(user, actualUser);
+    }
 
-        User savedUser = userService.saveUser(user);
-        assertNotNull(savedUser);
-        assertEquals(user, savedUser);
+    @Test
+    public void findByIdTest() {
+        User expectedUser = new User("firstName", "lastName", "email");
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(expectedUser));
+        Optional<User> actualUser = userService.findById("id");
+        verify(userRepository, times(1)).findById(anyString());
+        assertEquals(expectedUser, actualUser.get());
     }
 
     @Test
     public void updateUserTest() {
-        User user = new User("Jana", "Kowalska","test@example1.com");
-        String id = "1";
-        when(userRepository.save(any(User.class))).thenReturn(user);
-
-        User updatedUser = userService.updateUser(id, user);
-        assertNotNull(updatedUser);
-        assertEquals(user, updatedUser);
+        User expectedUser = new User("firstName", "lastName", "email");
+        User existingUser = new User("existingFirstName", "existingLastName", "existingEmail");
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(existingUser)).thenReturn(expectedUser);
+        User actualUser = userService.updateUser("id", expectedUser);
+        verify(userRepository, times(1)).findById(anyString());
+        verify(userRepository, times(1)).save(existingUser);
+        assertEquals(expectedUser, actualUser);
     }
 
     @Test
     public void findByEmailTest() {
-        User user = new User("Janka", "Kowalskaja","test@example2.com");
-        when(userRepository.findByEmail(any(String.class))).thenReturn(user);
-
-        User foundUser = userService.findByEmail("test@example2.com");
-        assertNotNull(foundUser);
-        assertEquals(user, foundUser);
-    }
-
-    @Test
-    public void deleteUserTest() {
-        User user = new User("Anna", "Kowal","test@example4.com");
-        userService.deleteUser(user.getId());
-    }
-
-    @Test
-    public void getAllUsersTest() {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Karp", "Kowalskij","test@example5.com"));
-        users.add(new User("Akas", "Kowa","test@example6.com"));
-        when(userRepository.findAll()).thenReturn(users);
-
-        List<User> allUsers = userService.getAllUsers();
-        assertEquals(users,allUsers);
-
+        User expectedUser = new User("firstName", "lastName", "email");
+        when(userRepository.findByEmail(anyString())).thenReturn(expectedUser);
+        User actualUser = userService.findByEmail("email");
+        verify(userRepository, times(1)).findByEmail(anyString());
+        assertEquals(expectedUser, actualUser);
     }
 }
