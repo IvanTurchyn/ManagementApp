98237ee660fcbde2081332430183fb1ff1668de7
IvanTurchyn <ivan.turchyn@interia.pl>
2023-02-04 14:06:29 +0100

    Added configuration THYMELEAF

--- src/main/java/com/example/ManagementApp/config/WebConfig.java
+++ src/main/java/com/example/ManagementApp/config/WebConfig.java
@@ -1,11 +1,19 @@
 package com.example.ManagementApp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 import java.util.List;
 import org.springframework.http.converter.HttpMessageConverter;
 import java.util.Arrays;
@@ -13,7 +21,6 @@ import org.springframework.http.MediaType;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -27,6 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
         jsonConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON));
         converters.add(jsonConverter);
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -41,4 +49,30 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new MyCustomInterceptor());
     }
 
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine((ISpringTemplateEngine) templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setViewNames(new String[]{"*.html"});
+        return viewResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
 }
