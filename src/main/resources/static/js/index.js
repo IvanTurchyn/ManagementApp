79108be231928f1f5691e91da49451874e50c258
IvanTurchyn <ivan.turchyn@ineria.pl>
2023-03-24 12:34:25 +0100

    Updated baseUrl in index.js file
    The baseUrl in the index.js file was updated from http://40.81.16.213 to http://localhost:8080.
    The code for fetching user data from the backend was also updated to use the fetch API instead of jQuery AJAX calls.
    Additionally, event listeners for delete, edit, and view buttons were added to the document object
    to improve the performance and readability of the code.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,85 +1,65 @@
-var baseUrl = "http://40.81.16.213";
+var baseUrl = "http://localhost:8080";
 
-$(document).ready(function() {
-let userList = $('#userList');
-$.ajax({
-url: baseUrl + "/users",
-type: "GET",
-dataType: "json",
-success: function(data) {
-// Iterujemy przez tablicę użytkowników
-$.each(data, function(index, user) {
-// Tworzymy nowy wiersz i dodajemy go do tabeli
-let tr = $('<tr>');
-tr.append(`<td>${user.firstName}</td>`);
-tr.append(`<td> ${user.lastName}</td>`);
-tr.append(`<td>${user.email}</td>`);
-tr.append(`<td> <button class="btn btn-primary mr-2 edit-user" data-id="${user.id}">Edytuj</button> <button class="btn btn-danger mr-2 delete-user" data-id="${user.id}">Usuń</button> <button class="btn btn-primary mr-2 view-user" data-id="${user.id}">Podgląd</button> </td>`);
-userList.append(tr);
-});
-},
-error: function(error) {
-console.log(error);
-}
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const userList = document.getElementById("userList");
 
-// Obsługa kliknięcia przycisku Usuń
-$(document).on('click', '.delete-user', function(event) {
-event.preventDefault();
-let id = $(this).data('id');
-let row = $(this).closest('tr');
-let firstName = row.find('td:first').text();
-let lastName = row.find('td:eq(1)').text();
-let name = firstName + " " + lastName;
-if (confirm("Czy napewno chcesz usunąć dane użytkownika " + name + "?")) {
-$.ajax({
-url: baseUrl + "/users/" + id,
-type: "DELETE",
-success: function() {
-row.remove();
-// Dodanie zapytania do serwera, które usunie dane użytkownika z bazy danych
-$.ajax({
-url: baseUrl + "/users/" + id,
-type: "DELETE",
-success: function() {
-alert("Użytkownik został usunięty z bazy danych");
-},
-error: function(error) {
-console.log(error);
-}
-});
-},
-error: function(error) {
-console.log(error);
-}
-});
-}
-});
+  fetch(baseUrl + "/users")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((user) => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.email}</td>
+                        <td><button class="btn btn-primary mr-2 edit-user" data-id="${user.id}">Edytuj</button>
+                            <button class="btn btn-danger mr-2 delete-user" data-id="${user.id}">Usuń</button>
+                            <button class="btn btn-primary mr-2 view-user" data-id="${user.id}">Podgląd</button></td>`;
+        userList.appendChild(tr);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-//Przekierowywanie do pliku "addUser.html"
-document.getElementById("addUser").addEventListener("click", function() {
-window.location.href = "addUser.html";
-});
-});
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete-user")) {
+      const id = event.target.getAttribute("data-id");
+      const row = event.target.closest("tr");
+      const firstName = row.cells[0].innerText;
+      const lastName = row.cells[1].innerText;
+      const name = firstName + " " + lastName;
+      if (confirm("Czy napewno chcesz usunąć dane użytkownika " + name + "?")) {
+        fetch(baseUrl + "/users/" + id, {
+          method: "DELETE",
+        })
+          .then(() => {
+            row.remove();
+            alert("Użytkownik został usunięty z bazy danych");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  });
 
-$(document).ready(function() {
-// Obsługa kliknięcia przycisku "Edytuj"
-$('#userList').on('click', '.edit-user', function() {
-// Pobieranie ID użytkownika z atrybutu "data-id" przycisku
-let userId = $(this).data('id');
-// Przekierowywanie do strony edycji użytkownika
-location.href = "editUser.html?id=" + userId;
-});
-});
+  document.getElementById("addUser").addEventListener("click", function () {
+    window.location.href = "addUser.html";
+  });
 
-$(document).ready(function() {
-// Obsługa kliknięcia przycisku "Podgląd"
-$('#userList').on('click', '.view-user', function() {
-// Pobieranie ID użytkownika z atrybutu "data-id" przycisku
-let userId = $(this).data('id');
-// Przekierowywanie do strony edycji użytkownika
-location.href = "viewDataUser.html?id=" + userId;
-});
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("edit-user")) {
+      const userId = event.target.getAttribute("data-id");
+      location.href = "editUser.html?id=" + userId;
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("view-user")) {
+      const userId = event.target.getAttribute("data-id");
+      location.href = "viewDataUser.html?id=" + userId;
+    }
+  });
 });
 
 
