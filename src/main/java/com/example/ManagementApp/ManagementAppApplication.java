41cb02c0e323e9d4296eb7584bb5ed3510eee2d2
IvanTurchyn <ivan.turchyn@ineria.pl>
2023-03-24 12:14:02 +0100

    Enable MongoDB repositories in ManagementAppApplication
    This commit updates the ManagementAppApplication class to enable MongoDB repositories using the @EnableMongoRepositories annotation. The @Import annotation has been removed, as it is no longer needed. The @Value annotation has been updated to make the port field private, and a new instance of the ManagementAppApplication class is created to retrieve the port value in the setDefaultProperties method.
    This change enables the use of MongoDB repositories in the application and makes the port value accessible to the setDefaultProperties method, which sets the default server port for the application. No other changes were made in this commit.

--- src/main/java/com/example/ManagementApp/ManagementAppApplication.java
+++ src/main/java/com/example/ManagementApp/ManagementAppApplication.java
@@ -1,30 +1,25 @@
 package com.example.ManagementApp;
-import com.example.ManagementApp.config.MongoDBConfig;
+
 import com.example.ManagementApp.config.WebConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.Collections;
 
 @Import(WebConfig.class)
-
-
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableMongoRepositories(basePackages = "com.example.ManagementApp.repository")
 public class ManagementAppApplication {
 	@Value("${server.port}")
-	static
-	int port;
+	private int port;
+
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.register(MongoDBConfig.class);
-		context.refresh();
 		SpringApplication app = new SpringApplication(ManagementAppApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(port)));
+		ManagementAppApplication managementAppApplication = new ManagementAppApplication();
+		app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(managementAppApplication.port)));
 		app.run(args);
 	}
 }
